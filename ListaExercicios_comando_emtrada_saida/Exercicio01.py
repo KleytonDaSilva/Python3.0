4fa377eec829839ebc1a307977ce171eef5a7e06
nksaddam <nksaddam@gmail.com>
2023-05-08 17:36:24 -0300

    08/05/2023

---
+++ ListaExercicios_comando_emtrada_saida/Exercicio01.py
@@ -0,0 +1,17 @@
+
+# Exercicios 01  lista
+
+print("_______________________________________________________")
+print("100 Francos franceses      |     21.55 Dólar canadenses")
+print("1 Dólar Americano          |     1.06  Dólar canadenses")
+print("100 Marcos alemães         |     43.20 Dólar canadenses")
+print("1 Libra Inglesa            |     1.84  Dólar canadenses")
+print("100 Coroas Suecas          |     24.25 Dólar canadenses")
+print("100 Dracmas Gregos         |     2.95  Dólar canadenses")
+print("_______________________________________________________")
+
+total = 0
+print("Conversão de moedas")
+franco = float(input("Informe a quantidade de Francos para converter em Dólar canadense"))
+franco = franco * 0.2155
+print("Quantidade Franco Franceses : ", franco)
