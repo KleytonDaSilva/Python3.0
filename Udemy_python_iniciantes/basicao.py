26ed89c943b2b4c4af104764a19209e14bf93a3e
nksaddam <nksaddam@gmail.com>
2023-06-04 20:12:38 -0300

    04/06/2023

---
+++ Udemy_python_iniciantes/basicao.py
@@ -0,0 +1,38 @@
+# primeira instrução
+
+print("Hello World!!")
+
+#variaveis
+
+a = 1
+b = 'String'
+c = ['lista', 'lista2', 'lista3']
+print(a)
+
+#operadores
+d = 1
+e = 3
+
+f = d + e
+f = d / e
+f = d * e
+f = d - e
+
+#operadores condicionais
+
+#1 == 1
+#1 != 1
+#2 >= 1
+#2 <= 1
+
+#Pratica
+
+primeiro =  1
+segundo = 4
+soma = primeiro + segundo * primeiro - segundo / 2
+print(soma)
+print(1 == 1)
+print(1 != 1)
+print(1 > 1)
+print(1 < 2)
+
---
+++ Udemy_python_iniciantes/lista.py
@@ -0,0 +1,48 @@
+# Listas
+
+lista = [2,3,1,2,'teste']
+
+print(lista)
+
+
+#item espeficico da lista, -1 sempre o ultimo da lista
+print(lista[0])
+print(lista[1])
+print(lista[-1])
+
+# removendo item da lista
+
+lista.remove('teste')
+print(lista)
+
+#pedaço da lista
+print("------------------")
+lista1 = [1,2,3,4,5,6]
+print(lista1)
+print(lista1[1:-1])
+print(lista1[1:])
+print(lista1[2:-2])
+
+#matriz
+print("-----------------")
+
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+print(matriz)
+print(matriz[0])
+print(matriz[1])
+print(matriz[0] [0])#primeira linha primeira coluna
+
+#somar uma matriz com a outra
+#sorted
+
+#pratica
+
+lista3 = ['teatro', 'valores', 123]
+print(lista3)
+lista3.append(455) #adiciona
+print(lista3)
+
+print(lista3[-1])
+lista3.remove(123)
+print(lista3)
+
